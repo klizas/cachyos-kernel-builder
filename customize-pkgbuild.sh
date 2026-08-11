afb25d0ea7c2938736160eef6c29f194b23b397d
klizas <lukas@klizas.lt>
2026-08-11 19:54:39 +0300

    Add 0001-xhci-pci-t2-titan-ridge-no-runtime-pm.patch

--- customize-pkgbuild.sh
+++ customize-pkgbuild.sh
@@ -54,8 +54,9 @@ source+=(
     "0001-touchbar-suspend-resume.patch"
     "0001-x86-resume-cached-cpu-bringup.patch"
     "0001-macsmc-hwmon-resume-null-deref.patch"
+    "0001-xhci-pci-t2-titan-ridge-no-runtime-pm.patch"
 )
-sha256sums+=(SKIP SKIP SKIP SKIP SKIP)
+sha256sums+=(SKIP SKIP SKIP SKIP SKIP SKIP)
 PATCH_SOURCES
 
 # --- 3. Add apple-bce-drv build to build() ---
